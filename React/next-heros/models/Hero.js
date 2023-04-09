00db09d6bdfde8b84435ea03d100158b569b27f8
Kehinde Onifade <onifkay@gmail.com>
2023-04-09 21:08:30 +0100

    create model

---
+++ React/next-heros/models/Hero.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const HeroSchema = new mongoose.Schema({
+  superhero: {
+    type: String,
+    required: [true, "Please name the hero"],
+    unique: true,
+    minlength: [3, "Name must be at least 3 characters long"],
+    trim: true,
+  },
+  realName: {
+    type: String,
+    required: [true, "Please name the hero"],
+    maxLength: [150, "Please keep real name short"],
+  }
+});
